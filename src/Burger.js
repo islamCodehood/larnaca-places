1ce79f31db85047a2cc87f7079dc02db0150fddd
Islam <islam.sayed8@gmail.com>
2018-07-26 11:35:18 +0200

    test: Add placeholder code

--- src/Burger.js
+++ src/Burger.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react'
 
-
+class Burger extends Component {
+    render() {
+        return (
+            <div> Burger </div>
+        )
+    }
+}
 
 export default Burger
