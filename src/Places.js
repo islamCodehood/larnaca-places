270a6e2c12421109318900927faa59592eab83ac
Islam <islam.sayed8@gmail.com>
2018-07-27 01:26:05 +0200

    fix: Remove a component

--- src/Places.js
+++ src/Places.js
@@ -1,15 +1,14 @@
 import React, { Component} from 'react'
 import Header from './Header'
 import SearchForm from './SearchForm'
-import List from './List'
 
 class Places extends Component {
+
     render() {
         return (
             <div>
                 <Header />
-                <SearchForm />
-                <List />
+                <SearchForm markers={this.props.markers} testMe={this.props.testMe} />
             </div>
         )
     }
