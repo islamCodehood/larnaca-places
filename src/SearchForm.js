84cac62df50495070061dcb6368b9810d75db115
Islam <islam.sayed8@gmail.com>
2018-07-27 01:26:29 +0200

    fix: Add places list to component

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,12 +1,54 @@
-import React, { Component} from 'react'
+import React, { Component } from "react";
+import escapeRegExp from "escape-string-regexp";
+import sortBy from "sort-by";
 
 class SearchForm extends Component {
-    render() {
-        return (
-            <div>SearchForm</div>
-        )
+  state = {
+    query: ""
+  };
+  handleChange = (query, arr) => {
+    this.setState({
+      query: query.trim()
+    });
+  };
+
+  render() {
+    let matchedPlaces;
+    if (this.state.query) {
+      const searchText = new RegExp(escapeRegExp(this.state.query), "i");
+      matchedPlaces = this.props.markers.filter(marker =>
+        searchText.test(marker.title)
+      );
+    } else {
+      matchedPlaces = this.props.markers;
     }
+    matchedPlaces.sort(sortBy("title"));
+
+    console.log(matchedPlaces);
+    return (
+      <div>
+        <div id="searchForm">
+          <form>
+            <input
+              type="text"
+              id="seacrh-text-input"
+              placeholder="Enter a place"
+              value={this.state.query}
+              onChange={evt => this.handleChange(evt.target.value)}
+            />
+            <input type="button" id="filter-button" value="Filter" />
+          </form>
+        </div>
+        <div id="list">
+          <ul id="unordered-list">
+            {matchedPlaces.map(marker => (
+              <li key={marker.id}>{marker.title}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
-
-export default SearchForm
+export default SearchForm;
