165180fd11a2bb304ae5208f8f299825abefdb0b
Islam <islam.sayed8@gmail.com>
2018-07-27 23:37:19 +0200

    fix: Add placesLocations props

--- src/Places.js
+++ src/Places.js
@@ -10,6 +10,7 @@ class Places extends Component {
         <SearchForm
           listedPlaces={this.props.listedPlaces}
           testMe={this.props.testMe}
+          placesLocations={this.props.placesLocations}
         />
       </div>
     );
