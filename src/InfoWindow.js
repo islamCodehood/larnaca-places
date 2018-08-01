2b49acebaa7c5930a8a551a1a2d376fab548b87b
Islam <islam.sayed8@gmail.com>
2018-08-01 14:07:22 +0200

    fix: Add image element

--- src/InfoWindow.js
+++ src/InfoWindow.js
@@ -1,20 +1,13 @@
 import React, { Component } from "react";
 
 class InfoWindow extends Component {
-    
-    handleClick = (evt) => {
-        const param = [this.props.title, this.props.lat, this.props.lng]
-        const [title, lat, lng] = param
-        console.log(evt.target)
-        this.props.getId(title, lat, lng)
-        //this.props.getDetails()
-    }
     render() {
       return (
         <div>
             <div id="title">{this.props.title}</div>
-            <div id="address">Cyprus</div>
-            <button onClick={this.handleClick}>read more</button>
+            <img src={this.props.bestPhoto} alt=""/>
+            <div id="category"><span>Category: </span>{this.props.category}</div>
+            <div id="location"><span>Location: </span> {this.props.address}</div>
         </div>
       );
     }
