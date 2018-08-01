ff6196251429e9799ae3a0677731f43d79044f15
Islam <islam.sayed8@gmail.com>
2018-08-02 00:27:10 +0200

    fix: Add header

--- src/Burger.js
+++ src/Burger.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Burger extends Component {
     render() {
         return (
-            <div id="burger" onClick={this.props.handleClick}> &#9776; </div>
+            <div id="burger" onClick={this.props.handleClick}> &#9776; <span id="burger-header">Larnaca Places</span></div>
         )
     }
 }
