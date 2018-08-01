243cae46a56e0495a0622a56968e91e9ff6fe20f
Islam <islam.sayed8@gmail.com>
2018-08-01 14:06:58 +0200

    fix: Add id

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React, { Component} from 'react'
 class Header extends Component {
     render() {
         return (
-            <header>Larnaca Places</header>
+            <header id="header">Larnaca Places</header>
         )
     }
 }
