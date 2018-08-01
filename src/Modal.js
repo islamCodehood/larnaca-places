551b9381b626a554fcea33f3f01bb97bd25aacd3
Islam <islam.sayed8@gmail.com>
2018-08-02 00:26:19 +0200

    fix: Add props

--- src/Modal.js
+++ src/Modal.js
@@ -5,11 +5,12 @@ class Modal extends Component {
     render() {
       return (
         <div id="modal">
-          <div id="title"></div>
+          <div id="title">{this.props.title}</div>
           <img src="" alt=""/>
-          <div id="address"></div>
-          <div id="likes"></div>
-          <div id="hours"></div>
+          <div id="location"><span>Location: </span>{this.props.address}</div>
+          <div id="likes"><span>Likes: </span>{this.props.likes}</div>
+          <div id="rating"><span>Rating: </span>{this.props.rating}</div><div id="rating-color" style={{background: `#${this.props.color}`}}></div>
+          <div id="hours"><span>Hours: </span>{this.props.hours}</div>
         </div>
       );
     }
