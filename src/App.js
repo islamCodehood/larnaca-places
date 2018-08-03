222bdb8f11857be42f6f708b2cc37d245a127b70
Islam <islam.sayed8@gmail.com>
2018-08-03 18:45:50 +0200

    fix: Add an alternative image

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import escapeRegExp from "escape-string-regexp";
 import sortBy from "sort-by";
 import ReactDOM from "react-dom";
 import InfoWindow from "./InfoWindow";
+import cyprusflag from './cyprusflag.png'
 
 class App extends Component {
   state = {
@@ -25,8 +26,8 @@ class App extends Component {
     //to store the currently used infoWindo
     infoWindows: [],
     //client id and client secret to be used in fetching api from foursquare api (unchanged)
-    clientId: "ZBPGPJ4YEZFLSXOFYWBMIWDYVA3I211NBJXN1T5ZCV3PEI0C",
-    clientSecret: "5GWJZPVSPM5XYL1CMQQK1J1YW2QQGQKADZ5VTYQISTWYC4TX",
+    clientId: "0FSPUJ55OAZ2XIZDWO54KRR5P4KYOGNW2MC21JPHGIQIJ0LG",
+    clientSecret: "2GY12T2KTPRUUPYZYTEJATVFOU00T1D1PYDCMSJ22K5AONRQ",
     //to store the current marker (place) formattedAddress tom be used
     address: "",
     placeId: "",
@@ -35,16 +36,16 @@ class App extends Component {
     bestPhoto: "",
     category: [],
     title: "",
-    likes: '',
-    hours: '',
-    rating: '',
-    ratingColor: ''
+    likes: "",
+    hours: "",
+    rating: "",
+    ratingColor: ""
   };
 
   componentDidMount() {
     //get places dynamically from foursquare api
     this.getPlaces();
-    
+
     /*setting time out to wait for the getPlaces to return a value and update places
     * state before loading map and markers*/
     setTimeout(() => {
@@ -68,7 +69,7 @@ class App extends Component {
       }
     }, 3000);
   }
-  
+
   componentDidUpdate() {
     ////Check if the query contains any text to search for places
     if (this.state.query) {
@@ -85,150 +86,145 @@ class App extends Component {
     }
     ///////////////////////////////////////////////////////////////
     if (this.state.selectedPlace) {
-      console.log(this.state.selectedPlace)
+      console.log(this.state.selectedPlace);
       this.state.listedPlaces.forEach(listedPlace => {
         if (listedPlace.title === this.state.selectedPlace) {
           var map = this.state.map;
           const place = this.state.places.find(
             place => place.name === this.state.selectedPlace
           );
-          this.showSelectedInfoWindow(listedPlace, map, place)
+          this.showSelectedInfoWindow(listedPlace, map, place);
           //////Animation part
           listedPlace.setAnimation(window.google.maps.Animation.BOUNCE);
           setTimeout(() => {
             listedPlace.setAnimation(null);
           }, 400);
-          
         }
       });
       this.setState({
         selectedPlace: ""
       });
     }
-    //////////////////////////////////////////////////////////////////////////
-  
   }
   selectPlace = selectedPlace => {
     this.setState({
       selectedPlace
     });
   };
-//initiate map and call makeMarkers()
+  //initiate map and call makeMarkers()
   onScriptLoad = () => {
+    //Styles from https://snazzymaps.com/style/18/retro
     var styles = [
       {
-          "featureType": "administrative",
-          "stylers": [
-              {
-                  "visibility": "off"
-              }
-          ]
+        featureType: "administrative",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
       },
       {
-          "featureType": "poi",
-          "stylers": [
-              {
-                  "visibility": "simplified"
-              }
-          ]
+        featureType: "poi",
+        stylers: [
+          {
+            visibility: "simplified"
+          }
+        ]
       },
       {
-          "featureType": "road",
-          "elementType": "labels",
-          "stylers": [
-              {
-                  "visibility": "simplified"
-              }
-          ]
+        featureType: "road",
+        elementType: "labels",
+        stylers: [
+          {
+            visibility: "simplified"
+          }
+        ]
       },
       {
-          "featureType": "water",
-          "stylers": [
-              {
-                  "visibility": "simplified"
-              }
-          ]
+        featureType: "water",
+        stylers: [
+          {
+            visibility: "simplified"
+          }
+        ]
       },
       {
-          "featureType": "transit",
-          "stylers": [
-              {
-                  "visibility": "simplified"
-              }
-          ]
+        featureType: "transit",
+        stylers: [
+          {
+            visibility: "simplified"
+          }
+        ]
       },
       {
-          "featureType": "landscape",
-          "stylers": [
-              {
-                  "visibility": "simplified"
-              }
-          ]
+        featureType: "landscape",
+        stylers: [
+          {
+            visibility: "simplified"
+          }
+        ]
       },
       {
-          "featureType": "road.highway",
-          "stylers": [
-              {
-                  "visibility": "off"
-              }
-          ]
+        featureType: "road.highway",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
       },
       {
-          "featureType": "road.local",
-          "stylers": [
-              {
-                  "visibility": "on"
-              }
-          ]
+        featureType: "road.local",
+        stylers: [
+          {
+            visibility: "on"
+          }
+        ]
       },
       {
-          "featureType": "road.highway",
-          "elementType": "geometry",
-          "stylers": [
-              {
-                  "visibility": "on"
-              }
-          ]
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [
+          {
+            visibility: "on"
+          }
+        ]
       },
       {
-          "featureType": "water",
-          "stylers": [
-              {
-                  "color": "#84afa3"
-              },
-              {
-                  "lightness": 52
-              }
-          ]
+        featureType: "water",
+        stylers: [
+          {
+            color: "#84afa3"
+          },
+          {
+            lightness: 52
+          }
+        ]
       },
       {
-          "stylers": [
-              {
-                  "saturation": -17
-              },
-              {
-                  "gamma": 0.36
-              }
-          ]
+        stylers: [
+          {
+            saturation: -17
+          },
+          {
+            gamma: 0.36
+          }
+        ]
       },
       {
-          "featureType": "transit.line",
-          "elementType": "geometry",
-          "stylers": [
-              {
-                  "color": "#3f518c"
-              }
-          ]
+        featureType: "transit.line",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#3f518c"
+          }
+        ]
       }
-  ]
-    var map = new window.google.maps.Map(
-      document.getElementById("map"),
-      {
-        center: { lat: 34.900253, lng: 33.623172 },
-        zoom: 13,
-        styles: styles
-      }
-    );
+    ];
+    var map = new window.google.maps.Map(document.getElementById("map"), {
+      center: { lat: 34.900253, lng: 33.623172 },
+      zoom: 13,
+      styles: styles
+    });
     this.setState({
       map
     });
@@ -249,7 +245,6 @@ class App extends Component {
         animation: window.google.maps.Animation.DROP
       });
       marker.addListener("click", function() {
-        
         showInfoWindow(this, map, place);
       });
       this.setState(state => ({
@@ -263,7 +258,6 @@ class App extends Component {
       bounds.extend(marker.position);
     });
     map.fitBounds(bounds);
-
 
     var infoWindow = new window.google.maps.InfoWindow({
       content: '<div id="infoWindow">'
@@ -289,21 +283,23 @@ class App extends Component {
             lng={marker.position.lng()}
             address={address}
             bestPhoto={this.getBestPhoto()}
-            id={place.id} 
+            id={place.id}
             category={this.getCategory()}
             likes={this.getLikes()}
+            rating={this.getRating()}
+            ratingColor={this.getRatingColor()}
           />,
           document.getElementById("infoWindow")
         );
-        //Citation for the styling of info window : 
+        //Citation for the styling of info window :
         //http://en.marnoto.com/2014/09/5-formas-de-personalizar-infowindow.html
-        const iwOuter = document.querySelector('.gm-style-iw')
-        const iwBackground = iwOuter.previousSibling
+        const iwOuter = document.querySelector(".gm-style-iw");
+        const iwBackground = iwOuter.previousSibling;
         if (iwBackground) {
-          iwBackground.parentElement.removeChild(iwBackground)
+          iwBackground.parentElement.removeChild(iwBackground);
         }
-        const iwCloseBtn = iwOuter.nextElementSibling
-        iwCloseBtn.classList.add('iw-close-btn')
+        const iwCloseBtn = iwOuter.nextElementSibling;
+        iwCloseBtn.classList.add("iw-close-btn");
       });
       infoWindow.open(map, marker);
     };
@@ -329,9 +325,20 @@ class App extends Component {
           bestPhoto={this.getBestPhoto()}
           category={this.getCategory()}
           likes={this.getLikes()}
+          rating={this.getRating()}
+          ratingColor={this.getRatingColor()}
         />,
         document.getElementById("infoWindow")
       );
+      //Citation for the styling of info window :
+      //http://en.marnoto.com/2014/09/5-formas-de-personalizar-infowindow.html
+      const iwOuter = document.querySelector(".gm-style-iw");
+      const iwBackground = iwOuter.previousSibling;
+      if (iwBackground) {
+        iwBackground.parentElement.removeChild(iwBackground);
+      }
+      const iwCloseBtn = iwOuter.nextElementSibling;
+      iwCloseBtn.classList.add("iw-close-btn");
     });
     selectedInfoWindow.open(map, listedPlace);
   };
@@ -362,8 +369,13 @@ class App extends Component {
 
   getPlaces = () => {
     //fetch places from foursquare api using search venue then store them in places state
+    /* fetch(
+      `https://api.foursquare.com/v2/venues/search?ll=34.900253,33.623172&radius=10000&intent=browse&limit=10&client_id=${
+        this.state.clientId
+      }&client_secret=${this.state.clientSecret}&v=20180730`
+    ) */
     fetch(
-      `https://api.foursquare.com/v2/venues/search?ll=34.900253,33.623172&radius=10000&intent=browse&limit=2&client_id=${
+      `https://api.foursquare.com/v2/venues/search?near=larnaca&intent=browse&limit=10&client_id=${
         this.state.clientId
       }&client_secret=${this.state.clientSecret}&v=20180730`
     )
@@ -387,77 +399,107 @@ class App extends Component {
               this.setState(state => ({
                 venues: state.venues.concat(data.response.venue)
               }));
-            }).catch(error => console.log)
+            })
+            .catch(error => console.log);
         })
-      ).catch(error => console.log(error))
+      )
+      .catch(error => console.log(error));
   };
 
   getBestPhoto = () => {
     if (this.state.placeId) {
-      const bestPhotoObject = this.state.venues.find(venue => venue.id === this.state.placeId).bestPhoto
-      const bestPhotoSize = '280x180'
-      if(bestPhotoObject) {
-        return bestPhotoObject.prefix
-          + bestPhotoSize 
-          + bestPhotoObject.suffix
+      const bestPhotoObject = this.state.venues.find(
+        venue => venue.id === this.state.placeId
+      ).bestPhoto;
+      const bestPhotoSize = "280x180";
+      if (bestPhotoObject) {
+        return bestPhotoObject.prefix + bestPhotoSize + bestPhotoObject.suffix;
       } else {
-        return ''
+        return cyprusflag;
       }
     } else {
-      return ''
+      return cyprusflag;
     }
-
-  }
+  };
 
   getCategory = () => {
-    const categoriesArray = this.state.venues.find(venue => venue.id === this.state.placeId).categories
-    if (categoriesArray) {
-      return categoriesArray.map(category => category.name).join(', ')
+    if (this.state.placeId) {
+      const categoriesArray = this.state.venues.find(
+        venue => venue.id === this.state.placeId
+      ).categories;
+      if (categoriesArray) {
+        return categoriesArray.map(category => category.name).join(", ");
+      } else {
+        return "No data available";
+      }
     } else {
-      return 'No Data Available'
+      return 'No data available. Please, try again later!'
     }
-  }
+  };
 
   getName = () => {
     if (this.state.placeId) {
-      const name = this.state.venues.find(venue => venue.id === this.state.placeId).name
-      return name
-    }
-  }
-
-  getAddress = () => {
-    if (this.state.placeId) {
-      const address = this.state.venues.find(venue => venue.id === this.state.placeId).location.formattedAddress.join(", ");
-      return address
+      const name = this.state.venues.find(
+        venue => venue.id === this.state.placeId
+      ).name;
+      return name;
     } else {
-      const address = 'No Data Availabla'
-      return address
+      return 'No data available. Please, try again later!'
     }
-  }
+  };
+
 
   getLikes = () => {
     if (this.state.placeId) {
-      const likes = this.state.venues.find(venue => venue.id === this.state.placeId).likes.summary
-      return likes
+      const likes = this.state.venues.find(
+        venue => venue.id === this.state.placeId
+      ).likes.summary;
+      if (likes) {
+        return likes;
+      } else {
+        return "No data available.";
+      }
     } else {
-      const likes = 'No Data Availabla'
-      return likes
+      return "No data available. Please, try again later!";
     }
-  }
+  };
 
   getRating = () => {
     if (this.state.placeId) {
-      const rating = this.state.venues.find(venue => venue.id === this.state.placeId)
+      const venue = this.state.venues.find(
+        venue => venue.id === this.state.placeId
+      );
+      const rating = venue.rating;
+      if (rating) {
+        return rating;
+      } else {
+        return "No data available.";
+      }
+    } else {
+      return "No data available. Please, try again later!";
     }
-  }
+  };
 
+  getRatingColor = () => {
+    if (this.state.placeId) {
+      const venue = this.state.venues.find(
+        venue => venue.id === this.state.placeId
+      );
+      const ratingColor = venue.ratingColor;
+      if (ratingColor) {
+        return "#" + ratingColor;
+      } else {
+        return "";
+      }
+    }
+  };
 
   openCloseDrawer = () => {
     const drawer = document.getElementById("places-section");
     const rightSection = document.getElementById("right-section");
     const searchArea = document.getElementById("search-area");
     const header = document.getElementById("header");
-    const burgerHeader = document.getElementById("burger-header")
+    const burgerHeader = document.getElementById("burger-header");
     if (drawer.clientWidth > 0) {
       drawer.style.width = 0;
       rightSection.style.width = "100%";
@@ -469,9 +511,9 @@ class App extends Component {
       header.style.transitionDuration = "0.2s";
       searchArea.style.transitionDelay = "0s";
       header.style.transitionDelay = "0s";
-      burgerHeader.style.transitionProperty ="visibility"
-      burgerHeader.style.transitionDuration = "1s"
-      burgerHeader.style.visibility = "visible"
+      burgerHeader.style.transitionProperty = "visibility";
+      burgerHeader.style.transitionDuration = "1s";
+      burgerHeader.style.visibility = "visible";
     } else {
       drawer.style.width = "25%";
       rightSection.style.width = "75%";
@@ -483,9 +525,9 @@ class App extends Component {
       header.style.transitionDuration = "0.8s";
       searchArea.style.transitionDelay = "0.7s";
       header.style.transitionDelay = "0.7s";
-      burgerHeader.style.transitionProperty ="visibility"
-      burgerHeader.style.transitionDuration = "0.7s"
-      burgerHeader.style.visibility = "hidden"
+      burgerHeader.style.transitionProperty = "visibility";
+      burgerHeader.style.transitionDuration = "0.7s";
+      burgerHeader.style.visibility = "hidden";
     }
   };
 
@@ -499,8 +541,7 @@ class App extends Component {
         />
         <div id="right-section">
           <Burger handleClick={this.openCloseDrawer} />
-          <Map/>
-
+          <Map />
         </div>
       </div>
     );
