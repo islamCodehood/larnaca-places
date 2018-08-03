51c76158694696a3dd26f6339d3958221d993891
Islam <islam.sayed8@gmail.com>
2018-08-03 18:45:18 +0200

    fix: Add rating section

--- src/InfoWindow.js
+++ src/InfoWindow.js
@@ -4,11 +4,14 @@ class InfoWindow extends Component {
     render() {
       return (
         <div>
-            <div id="title"><span>{this.props.title}</span></div>
             <img src={this.props.bestPhoto} alt=""/>
-            <div id="category"><span>Category: </span>{this.props.category}</div>
-            <div id="location"><span>Location: </span> {this.props.address}</div>
-            <div id="likes"><span>Likes: </span> {this.props.likes}</div>
+            <div id="title"><span>{this.props.title}</span></div>
+            <div id="info">
+              <div id="category"><span>{this.props.category}</span></div>
+              <div id="location"><span>Location </span> {this.props.address}</div>
+              <div id="likes"><span>Likes </span> {this.props.likes}</div>
+              <div id="rating"><span id="rating-title">Rating </span> <span id="ratingColor" style={{background: `${this.props.ratingColor}`}}>{this.props.rating}</span></div>
+            </div>
         </div>
       );
     }
