92e9638e67a4aef894a38b48d34f3e8628cd7773
Islam <islam.sayed8@gmail.com>
2018-08-05 10:01:16 +0200

    fix: Add class burger-header-disappear

--- src/Burger.js
+++ src/Burger.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 class Burger extends Component {
     render() {
         return (
-            <div id="burger"><span id="burger-icon" onClick={this.props.handleClick}>&#9776;</span>  <span id="burger-header">LARNACA Places</span></div>
+            <div id="burger">
+                <span id="burger-icon" onClick={this.props.handleClick}>&#9776;</span>
+                <span id="burger-header" className="burger-header-disappear">LARNACA Places</span>
+            </div>
         )
     }
 }
