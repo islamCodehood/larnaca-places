df41c9205c9d370f41b4331031d3acde4c66e82f
Islam <islam.sayed8@gmail.com>
2018-08-06 10:46:03 +0200

    fix: add onkeyDown props

--- src/Burger.js
+++ src/Burger.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
 
 class Burger extends Component {
+    handleKeyDown = (evt) => {
+        evt.preventDefault()
+        console.log(evt.keyCode)
+        this.props.openCloseDrawerByKeyDown(evt.keyCode)
+    }
     render() {
         return (
-            <section id="burger">
-                <span id="burger-icon" onClick={this.props.handleClick}>&#9776;</span>
-                <header id="burger-header" className="burger-header-disappear" tabIndex="0">LARNACA Places</header>
-            </section>
+            <div id="burger">
+                <span 
+                    id="burger-btn" 
+                    role="button" 
+                    aria-label={this.props.BurgerBtnLabel} 
+                    tabIndex="0" 
+                    onClick={this.props.handleClick}
+                    onKeyDown={this.handleKeyDown}
+                >&#9776;
+                </span>
+                <header 
+                    id="burger-header" 
+                    className="burger-header-disappear" 
+                    tabIndex="0"
+                >LARNACA Places
+                </header>
+            </div>
         )
     }
 }
