feea1ed3f8e5579e0c01b885b922b2db78f6731b
Islam <islam.sayed8@gmail.com>
2018-08-07 13:06:23 +0200

    fix: Change ids to classes

--- src/FailedMapRequest.js
+++ src/FailedMapRequest.js
@@ -3,11 +3,11 @@ import React from "react";
 //The component that appears only when request of google maps fails
 function FailedMapRequest() {
     return (
-        <div className="failed-map-request">
-            <div id="sad-face">:(</div>
+        <div className="failed-map-request" tabIndex="-1">
+            <div className="sad-face">:(</div>
             <div className="failure-message">Map is not available now. Please, <a href="localhost:3000">try again</a> in a minute or two.</div>
             <br/>
-            <address className="failure-message">If The problem presists, please <a href="mailto:islam.sayed8@gmail.com">contact me</a>.</address>
+            <address className="failure-message">If The problem persists, please <a href="mailto:islam.sayed8@gmail.com">contact me</a>.</address>
         </div>
     )
 }
--- src/FailedRequest.js
+++ src/FailedRequest.js
@@ -3,9 +3,9 @@ import React from "react";
 //The component that appears only when request of foursquare fails
 function FailedRequest() {
     return (
-        <div className="failed-request">
-            <div id="sad-face">:(</div>
-            <div id="failure-message">No data available now. Please, <a href="localhost:3000">try again</a> in a minute or two.</div>
+        <div className="failed-request" tabIndex="-1">
+            <div className="sad-face">:(</div>
+            <div className="failure-message">No data available now. Please, <a href="localhost:3000">try again</a> in a minute or two.</div>
         </div>
     )
 }
