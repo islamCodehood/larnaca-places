672f0cb96a141d7f869fa4ae570b87d1eb15be3c
Islam <islam.sayed8@gmail.com>
2018-08-07 02:51:33 +0200

    fix:change to functional comp

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,9 @@
-import React, { Component} from 'react'
+import React from 'react'
 
-class Header extends Component {
-    render() {
+function Header() {
         return (
             <header id="header"><h1>LARNACA Places</h1></header>
         )
-    }
 }
-
 
 export default Header
